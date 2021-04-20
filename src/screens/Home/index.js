1037d3ca82afb71ce4b7dc4aa24dc95569c82600
Ricardo Levi <ricardolevi2005@hotmail.com>
2021-04-19 23:56:06 -0300

    Atualizado até posição 3:50:41 do vídeo

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,102 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Platform} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
 
-import {Container} from './styles';
+import Api from '../../Api';
+
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+  LoadingIcon,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState('');
+  const [coords, setCoords] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handlerLocationFinder = async () => {
+    setCoords(null);
+    let result = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+    );
+
+    if (result == 'granted') {
+      setLoading(true);
+      setLocationText('');
+      setList([]);
+
+      Geolocation.getCurrentPosition(info => {
+        setCoords(info.coords);
+        getBarbers();
+      });
+    }
+  };
+
+  const getBarbers = async () => {
+    setLoading(true);
+    setList([]);
+
+    let res = await Api.getBarbers();
+    if (res.error === '') {
+      if (res.loc) {
+        setLocationText(res.loc);
+      }
+      setList(res.data);
+    } else {
+      alert('Erro: ' + res.error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getBarbers();
+  }, []);
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" height="26" fill="#fff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#fff"
+            value={locationText}
+            onChangeText={t => setLocationText(t)}
+          />
+          <LocationFinder onPress={handlerLocationFinder}>
+            <MyLocationIcon width="24" height="24" fill="#fff" />
+          </LocationFinder>
+        </LocationArea>
+
+        {loading && <LoadingIcon size="large" color="#fff" />}
+
+        
+      </Scroller>
     </Container>
   );
 };
